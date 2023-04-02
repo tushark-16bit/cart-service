b0803feedfc4d85deb4d1a846071e5a43d596b4d
tushark-16bit <kalra.tushar.16@gmail.com>
2023-04-02 22:12:08 +0530

    * Add Book Client And book Proxy implemented with Feign Client

--- src/main/java/com/tk16/microservices/cartservice/CartServiceApplication.java
+++ src/main/java/com/tk16/microservices/cartservice/CartServiceApplication.java
@@ -2,12 +2,13 @@ package com.tk16.microservices.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-public class CartServiceApplication {
+@EnableFeignClients
+@SpringBootApplication public class CartServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CartServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CartServiceApplication.class, args);
+    }
 
 }
