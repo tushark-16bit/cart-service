3ff0d8bb43e3750a9a07506cd55fd64ce39c4d13
tushark-16bit <kalra.tushar.16@gmail.com>
2023-04-19 14:28:29 +0530

    add spring docs swagger

--- src/main/java/com/tk16/microservices/cart_service/infrastructure/in/web/config/WebSecurityConfig.java
+++ src/main/java/com/tk16/microservices/cart_service/infrastructure/in/web/config/WebSecurityConfig.java
@@ -13,13 +13,13 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
             throws Exception {
-        http.authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .oauth2ResourceServer()
-                .jwt();
-
+                .oauth2ResourceServer().jwt();
         return http.build();
     }
 }
